7f7007f449628781fbb8c209f06444f9485522f5
Thayná da Silva Cota <thaynacota390@gmail.com>
2025-07-13 20:17:40 -0300

    Página de contato pronta

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express')
 
 const app = express();
 const PORT = 3000;
+const fs = require('fs');
 const path = require('path');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
@@ -12,6 +14,47 @@ app.get('/', (req, res) => {
 
 app.get('/contato', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'contato.html'));
+});
+
+app.post('/contato', (req, res) => {
+    const { nome, email, assunto, mensagem } = req.body;
+    
+    const novoContato = {
+        nome,
+        email,
+        assunto,
+        mensagem
+    };
+    
+    const arquivoContatos = path.join(__dirname, 'public', 'data', 'contatos.json');
+    console.log('Caminho absoluto do arquivo:', arquivoContatos); 
+    
+    try {
+        let contatos = [];
+        if (fs.existsSync(arquivoContatos)) {
+            const dados = fs.readFileSync(arquivoContatos, 'utf-8');
+            contatos = JSON.parse(dados);
+        }
+        
+        contatos.push(novoContato);
+        
+        fs.writeFileSync(arquivoContatos, JSON.stringify(contatos, null, 2));
+        console.log('Dados salvos com sucesso em:', arquivoContatos);
+        
+        res.redirect('/contato/agradecimento');
+    } catch (err) {
+        console.error('Erro detalhado:', {
+            message: err.message,
+            code: err.code,
+            path: err.path,
+            stack: err.stack
+        });
+        res.status(500).send('Erro ao processar o formulário');
+    }
+});
+
+app.get('/contato/agradecimento', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'agradecimento.html'));
 });
 
 app.get('/sugestao', (req, res) => {
